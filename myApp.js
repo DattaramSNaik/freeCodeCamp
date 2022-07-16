e3c711b1c3be7c071f3bb0a384679733cac67a57
DattaramSNaik <dattaramn@valueaddsofttech.com>
2022-07-16 16:11:45 +0530

    body-parser changes

--- myApp.js
+++ myApp.js
@@ -1,15 +1,26 @@
 let express = require('express');
 let app = express();
 var bGround = require('fcc-express-bground');
+var bodyParser = require("body-parser")
 require('dotenv').config()
 app.use('/public', express.static(__dirname+ '/public'));
+
+//Middleware for getting method poth and ip address
 app.use(function(req,res,next){
     console.log(req.method+" "+req.path+" - "+req.ip);
     next();
 })
+//Middleware for getting method bodyParser Data after every request.
+app.use(function(req,res,next){
+    bodyParser.urlencoded({extended: false})
+    console.log(bodyParser);
+    next();
+})
+//serving html file
 app.get("/",function(req,res){
     res.sendFile(__dirname+"/views/index.html");
   })
+//serving json data
 app.get("/json",function(req,res){
    res.json({"message": "HELLO JSON"})
     // if(process.env.MESSAGE_STYLE==="uppercase"){
@@ -22,19 +33,19 @@ app.get("/json",function(req,res){
 function getCurrentDateTime(){
     return new Date().toString();
 }
+//get current time and handle outside function
 app.get("/now",function(req,res,next){
    req.time=getCurrentDateTime();
     next()
   },function(req,res) {
     res.json({time:req.time})
   })
-
+//get current params object 
 app.get("/:word/echo",function(req,res){
     res.json({echo: req.params.word})
 })
-
+//get current query object 
 app.get("/name",function(req,res){
-    
      res.json({name: req.query.first+" "+req.query.last})
 })
 

f4aa291173f1cd4fc8660146bd1bff1843c3fcdf
DattaramSNaik <dattaramn@valueaddsofttech.com>
2022-07-16 14:26:01 +0530

    myApp Chanhges

--- myApp.js
+++ myApp.js
@@ -11,12 +11,12 @@ app.get("/",function(req,res){
     res.sendFile(__dirname+"/views/index.html");
   })
   app.get("/json",function(req,res){
-   
-    if(process.env.MESSAGE_STYLE==="uppercase"){
-      res.json({"message": "HELLO JSON"});
-    }else{
-      res.json({"message": "Hello json"});
-    }
+   res.json({"message": "HELLO JSON"})
+    // if(process.env.MESSAGE_STYLE==="uppercase"){
+    //   res.json({"message": "HELLO JSON"});
+    // }else{
+    //   res.json({"message": "Hello json"});
+    // }
     
   })
 

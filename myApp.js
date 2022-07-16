fd90ff2e83312c8870b9838f22bcc3b0d8d3faea
DattaramSNaik <dattaramn@valueaddsofttech.com>
2022-07-16 13:57:23 +0530

    first commit

---
+++ myApp.js
@@ -0,0 +1,23 @@
+let express = require('express');
+let app = express();
+var bGround = require('fcc-express-bground');
+require('dotenv').config()
+app.use('/public', express.static(__dirname+ '/public'));
+app.use(function(req,res,next){
+    console.log(req.method+" "+req.path+" - "+req.ip);
+    next();
+})
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/views/index.html");
+  })
+  app.get("/json",function(req,res){
+   
+    if(process.env.MESSAGE_STYLE==="uppercase"){
+      res.json({"message": "HELLO JSON"});
+    }else{
+      res.json({"message": "Hello json"});
+    }
+    
+  })
+
+module.exports = app;
